dcc8501ef5852ccb4d730e5a1ee66413e7c9756a
jhuffaker1 <jordan_huffaker@hotmail.com>
2021-06-14 18:37:51 -1000

    about to add a static class to polynomial save point

--- src/OrderedList.java
+++ src/OrderedList.java
@@ -1,9 +1,10 @@
 public class OrderedList {
 
     public boolean checkSorted(Polynomial[] polynomialList) {
-        boolean isSorted = true;
+        boolean isSorted = false;
         for (int i = 0; i < polynomialList.length - 1; i++) {
             int myChecker = polynomialList[i].compareTo(polynomialList[i + 1]);
+            System.out.println(myChecker);
             if (myChecker > 0) {
                     isSorted = this.checkSorted(polynomialList[i], polynomialList[i + 1]);
             }
@@ -16,6 +17,7 @@ public class OrderedList {
     }
 
     public static boolean checkSorted(Polynomial p1, Polynomial p2) {
+        System.out.println("doing a print");
         Polynomial.WeakOrderChecker weakChecker = new Polynomial.WeakOrderChecker();
         int myChecker = weakChecker.compare(p1,p2);
         System.out.println("made it here 003");
